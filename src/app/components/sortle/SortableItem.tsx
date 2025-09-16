d58c83239962d417b27807b7b72d926f8cd72e9a
hyoka_7 <fujisada.hayuma.w0@tufs.ac.jp>
2025-09-16 09:12:14 +0900

    テスト

--- src/app/components/sortle/SortableItem.tsx
+++ src/app/components/sortle/SortableItem.tsx
@@ -19,7 +19,7 @@ export default function SortableItem({ id, name, className }: Props) {
             {...attributes}
             {...listeners}
             className={clsx(
-                "p-2 m-1 rounded-md cursor-grab min-w-[80px] text-center border-2 w-auto break-words",
+                "p-2 m-1 rounded-md cursor-grab min-w-[80px] text-center border-2 w-auto break-words touch-none",
                 "bg-white dark:bg-gray-700 border-gray-400 dark:border-gray-500 text-gray-900 dark:text-gray-100",
                 className
             )}
